84e8d282acbc0363c41b0618683df5d66fcd9265
Emerson Sampaio <emersonsmp@gmail.com>
2020-07-02 17:30:15 -0300

    Elegant yoga Login

---
+++ Elegant_Yoga.Android/Control/CustomEntryRenderer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Elegant_Yoga.Control;
+using Elegant_Yoga.Droid.Control;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+
+[assembly: ExportRenderer(typeof(EntryClean), typeof(CustomEntryRenderer))]
+namespace Elegant_Yoga.Droid.Control
+{
+    class CustomEntryRenderer : EntryRenderer
+    {
+        public CustomEntryRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement == null)
+            {
+                Control.Background = null;
+            }
+        }
+    }
+}
---
+++ Elegant_Yoga/Control/EntryClean.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Elegant_Yoga.Control
+{
+    public class EntryClean : Entry
+    {
+        public EntryClean()
+        {
+        }
+    }
+}
